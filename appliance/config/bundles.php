4754673e4fb5a8a533156497291c4771773c7518
Richard Deloge <richarddeloge@gmail.com>
2023-05-17 18:28:25 +0200

    Update libs, Remove deprecated

--- appliance/config/bundles.php
+++ appliance/config/bundles.php
@@ -1,26 +1,5 @@
 <?php
 
-/*
- * Teknoo Space.
- *
- * LICENSE
- *
- * This source file is subject to the MIT license
- * it is available in LICENSE file at the root of this package
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to richard@teknoo.software so we can send you a copy immediately.
- *
- *
- * @copyright   Copyright (c) EIRL Richard Déloge (https://deloge.io - richard@deloge.io)
- * @copyright   Copyright (c) SASU Teknoo Software (https://teknoo.software - contact@teknoo.software)
- *
- * @link        http://teknoo.space Project website
- *
- * @license     http://teknoo.software/license/mit         MIT License
- * @author      Richard Déloge <richard@teknoo.software>
- */
-
 return [
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Teknoo\DI\SymfonyBridge\DIBridgeBundle::class => ['all' => true],
