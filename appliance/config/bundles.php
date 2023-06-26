df664f98a81cd9dfc388563c7d60cd7cbee1c969
Richard Deloge <richarddeloge@gmail.com>
2023-06-26 09:57:18 +0200

    ## [1.0.0-beta6] - 2023-06-26 ### Beta6 Release - Symfony 6.3.1 - Switch to PaaS Symfony metapackage

--- appliance/config/bundles.php
+++ appliance/config/bundles.php
@@ -1,15 +1,10 @@
 <?php
 
 return [
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
-    Teknoo\DI\SymfonyBridge\DIBridgeBundle::class => ['all' => true],
-    Teknoo\East\FoundationBundle\EastFoundationBundle::class => ['all' => true],
-    Teknoo\East\CommonBundle\TeknooEastCommonBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Teknoo\East\Paas\Infrastructures\EastPaasBundle\TeknooEastPaasBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
@@ -17,4 +12,9 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Teknoo\DI\SymfonyBridge\DIBridgeBundle::class => ['all' => true],
+    Teknoo\East\FoundationBundle\EastFoundationBundle::class => ['all' => true],
+    Teknoo\East\CommonBundle\TeknooEastCommonBundle::class => ['all' => true],
+    Teknoo\East\Paas\Infrastructures\EastPaasBundle\TeknooEastPaasBundle::class => ['all' => true],
 ];
