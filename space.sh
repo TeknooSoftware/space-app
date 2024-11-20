8d0a2341f4bb09005139cdbf7d8ad44724a2de0d
Richard Deloge <richarddeloge@gmail.com>
2024-11-20 16:58:30 +0100

    Update CLI tools

--- space.sh
+++ space.sh
@@ -2,4 +2,25 @@
 
 set -eu
 
-make -C appliance "$@"
+if [ "${1}" = "ext" ]; then
+  EXTENSION=$(echo "${2}" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
+
+  shift 2
+  if [ -x "appliance/extensions/${EXTENSION}/space.sh" ]; then
+    SCRIPT="appliance/extensions/${EXTENSION}/space.sh"
+    "$SCRIPT" "${@}"
+
+    exit 0
+  fi
+
+  if [ -e "appliance/extensions/${EXTENSION}/Makefile" ]; then
+    make --no-print-directory -C "appliance/extensions/${EXTENSION}" "${@}"
+    exit 0
+  fi
+
+  echo "Extension ${EXTENSION} has no Makefile or space.sh endpoint"
+
+  exit 1
+else
+  make --no-print-directory -C appliance "$@"
+fi
